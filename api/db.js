6e521f0d5ee5809546fdd8268d8894bfd6e10183
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2018-11-09 17:26:42 -0800

    Added sequelize, Message and MessageRecipient models

---
+++ api/db.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+const Message = sequelize.define('message', {
+    message_id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+    text: Sequelize.TEXT,
+    sender: Sequelize.INTEGER,
+    timestamp: Sequelize.INTEGER,
+    origin: Sequelize.ENUM('earth', 'mars'),
+    replicated: {type: Sequelize.BOOLEAN, defaultValue: false}
+});
+
+const MessageRecipient = sequelize.define('message_recipient', {
+    message_id: Sequelize.INTEGER,
+    recipient: Sequelize.INTEGER
+});
+
+module.exports = {sequelize: sequelize, Message: Message, MessageRecipient: MessageRecipient};
--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const {sequelize, Message, MessageRecipient} = require('./db.js');
 
 app.use(bodyParser.json());
 
-app.post('/message', function (req, res) {
+app.post('/message', (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
 });
 
-app.listen(3000);
+app.listen(3000, async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log('App ready on port 3000, DB synced')
+    } catch (ex) {
+        console.error(ex);
+    }
+});
