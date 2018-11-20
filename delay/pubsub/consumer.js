999c0f328ddac2744cefcbde2d0e393bf6049458
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2018-11-19 16:24:51 -0800

    Added redis instance for delay. Publishing keys with expiration to redis. Started work on redis expiration consumer.

--- delay/pubsub/consumer.js
+++ delay/pubsub/consumer.js
@@ -1,21 +1,28 @@
 const NatsStreaming = require('node-nats-streaming');
 const {DelayRpc} = require('./delayRpc');
+
+const Redis = require('ioredis');
+const redis = new Redis({host: process.env.REDIS_DELAY_HOST, port: process.env.REDIS_DELAY_PORT});
+
 const rpc = new DelayRpc('../protos/delay.proto');
 const stan = NatsStreaming.connect(process.env.NATS_CLUSTER, process.env.NATS_CLIENT, process.env.NATS_URL);
 
-stan.on('connect', () => {
-    const opts = stan.subscriptionOptions();
-    opts.setDurableName('delay-consumer');
-    const delaySub = stan.subscribe('delay', opts);
+redis.on('ready', () => {
+    stan.on('connect', () => {
+        const opts = stan.subscriptionOptions();
+        opts.setDurableName('delay-consumer');
+        const delaySub = stan.subscribe('delay', opts);
 
-    delaySub.on('message', (msg) => {
-        console.log(msg.getData());
+        delaySub.on('message', (msg) => {
+            const m = JSON.parse(msg.getData());
+            console.log(m);
 
-        rpc.getLightDelay(JSON.parse(msg.getData()).timestamp).then(res => {
-            console.log(res);
-        }).catch(ex => {
-            console.error(ex);
-        })
+            rpc.getLightDelay(m.timestamp).then(res => {
+                redis.set('key', m.message_id, 'ex', res.delay)
+            }).catch(ex => {
+                console.error(ex);
+            })
 
-    });
-});
+        });
+    })
+})
